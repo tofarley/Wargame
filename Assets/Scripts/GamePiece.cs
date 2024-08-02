da6ec5246faaed98aaeeaf6e4a6f2f04a4a44b2e
Tim Farley <tofarley@gmail.com>
2024-08-02 13:15:17 -0500

    Working Tilemap

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -1,26 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GamePiece : MonoBehaviour
 {
-    public Vector2Int hexCoordinates;
-    public GameObject currentHexTile;
-
-    public int moveDistance;
-    
-    // Initialize the GamePiece on a specific hex tile
-    public void Initialize(Vector2Int coordinates, GameObject hexTile)
+    // Start is called before the first frame update
+    void Start()
     {
-        hexCoordinates = coordinates;
-        currentHexTile = hexTile;
-        this.transform.position = hexTile.transform.position; // Align the game piece with the hex tile
+        
     }
 
-    void OnMouseDown()
+    // Update is called once per frame
+    void Update()
     {
-        // This code will be executed when the player clicks on the GameObject this script is attached to
-        Debug.Log("GamePiece clicked at X=" + hexCoordinates.x + " and Y=" + hexCoordinates.y);
-        // Do this as a unity setting
-        HexGrid hexGrid = FindObjectOfType<HexGrid>(); // Find the HexGrid in the scene
-        hexGrid.HighlightAdjacentHexes(hexCoordinates, true);
+        
     }
 }
